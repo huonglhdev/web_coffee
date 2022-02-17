5b19c66c79d779b8edc578035fbb3999478ff24b
Astius\Hau Huong <huonglhse140728@fpt.edu.vn>
2022-02-17 17:22:19 +0700

    Homepage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './images/coffee_logo.jpg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} style={{width:400,height:400}} alt="logo"/>
+        <div>
+          <h1 style={{letterSpacing:4, fontSize:46, margin:6}}>COMING SOON</h1>
+          <h5 style={{fontSize:22, margin:6,fontWeight:400}}>Công ty cổ phần Grati Coffee</h5>
+          <h6 style={{fontSize:14,margin:4, fontWeight:300}}>Hotline : 0918.79.68.73 - 0945.79.68.73</h6>
+        </div>
       </header>
     </div>
   );
